fa87b6eb816b9dd3e02f05fe4f17679c1d7ac39b
fumima-ru <marumefumiko@gmail.com>
2022-11-18 11:54:05 +0900

    jsファイル追加他

---
+++ js/sample_spice_herb_recipe1.js
@@ -0,0 +1,12 @@
+$(function() {
+  $('.slick-area').slick( {
+    autoplay: true,
+    /* arrows: true,
+    dots: true, */
+    // centerMode: false,
+    // Padding: '20px',
+    slidesToShow: 5,
+
+  });
+
+});

85065728bb34491a3318bb819654db21d258679c
fumima-ru <marumefumiko@gmail.com>
2022-11-28 14:42:42 +0900

    更新

--- js/sample_spice_herb_recipe1.js
+++ js/sample_spice_herb_recipe1.js
@@ -12,23 +12,26 @@ $(function() {
     $('.slick-area').slick({
       autoplay: true,
       dots: true,
+      arrows: false,
       centerMode: true,
       centerPadding: '100px',
       slidesToShow: 5,
       responsive: [
         {
-          breakpoint: 900,
+          breakpoint: 1400,
           settings: {
             centerPadding: '70px',
             slidesToShow: 3
           },
-          breakpoint: 768,
+        },
+        {
+          breakpoint: 800,
           settings: {
             centerPadding: '50px',
             slidesToShow: 1
-          }
-        }
-      ]
+          },
+        },
+      ],
     });
 
 });

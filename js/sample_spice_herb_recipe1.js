f69f16f859c8a6ba4d06ced9c0a5c6bebe1fa550
fumima-ru <marumefumiko@gmail.com>
2022-12-21 09:41:43 +0900

    必要のないコメントを削除

--- js/sample_spice_herb_recipe1.js
+++ js/sample_spice_herb_recipe1.js
@@ -1,37 +1,28 @@
 $(function() {
-  /* $('.slick-area').slick( {
+   
+  $('.slick-area').slick({
     autoplay: true,
     dots: true,
     arrows: false,
-    // centerMode: false,
-    // Padding: '20px',
+    centerMode: true,
+    centerPadding: '100px',
     slidesToShow: 5,
- */
-    
-      
-    $('.slick-area').slick({
-      autoplay: true,
-      dots: true,
-      arrows: false,
-      centerMode: true,
-      centerPadding: '100px',
-      slidesToShow: 5,
-      responsive: [
-        {
-          breakpoint: 1400,
-          settings: {
-            centerPadding: '70px',
-            slidesToShow: 3
-          },
+    responsive: [
+      {
+        breakpoint: 1400,
+        settings: {
+          centerPadding: '70px',
+          slidesToShow: 3
         },
-        {
-          breakpoint: 800,
-          settings: {
-            centerPadding: '50px',
-            slidesToShow: 1
-          },
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          centerPadding: '50px',
+          slidesToShow: 1
         },
-      ],
-    });
+      },
+    ],
+  });
 
 });
